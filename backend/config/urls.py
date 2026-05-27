b516a49670f7cf11e512ea15fba8d2cb8bfc498b
Ishika <ishikakv52@gmail.com>
2026-05-27 21:49:22 +0530

    changes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -24,11 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/', include('accounts.urls')),
-
-     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
-    path("", include("ImageToText.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('ImageToText.urls')),
     # path('voice-to-text/', include('VoiceToText.urls')),
-
 ]
