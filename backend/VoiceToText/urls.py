a765101efa8bfc1b170ec88cfbddda9ddcb22ab9
Ishika <ishikakv52@gmail.com>
2026-05-28 23:04:54 +0530

    VoiceToText Backend Completed

--- backend/VoiceToText/urls.py
+++ backend/VoiceToText/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from .views import index
 urlpatterns = [
     path("",                       views.index,                name="voicetotext_index"),
     path("transcribe/audio/",      views.transcribe_audio,     name="transcribe_audio"),
     path("transcribe/video/",      views.transcribe_video,     name="transcribe_video"),
     path("transcribe/microphone/", views.transcribe_microphone,name="transcribe_microphone"),
+     path('', index, name='voice_to_text'),
 ]
